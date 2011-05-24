06760dc20608e96355da3b0757f2771bef5c21af
J.S. Oishi <jsoishi@gmail.com>
2011-05-24 08:41:48 -0700

    no need for return statements.

--- python/init_cond.py
+++ python/init_cond.py
@@ -10,16 +10,11 @@ def taylor_green(ux, uy):
         uy.data[-1,1] = -1j/4.
         uy.data[1,-1] = 1j/4.
 
-    return ux,uy
-
 def sin_x(f,ampl=1.):
     f.data[0,1] = ampl*1j
     f.data[0,-1] = -f.data[0,1]
-
-    return f
 
 def sin_y(f,ampl=1.):
     f.data[1,0] = ampl*1j
     f.data[-1,0] = -f.data[0,1]
 
-    return f
