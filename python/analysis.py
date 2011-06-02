7c8913c100519c471a9009aa30ba3d566bf09e42
J.S. Oishi <jsoishi@gmail.com>
2011-06-02 16:01:29 -0700

    added energy spectrum calculator

--- python/analysis.py
+++ python/analysis.py
@@ -1,14 +1,14 @@
+import pylab as P
 import numpy as na
 import os
-def snapshot(data, it):
-    import pylab as P
 
+def snapshot(data, it):
     P.subplot(121)
     P.title('t = %5.2f' % data.time)
-    P.imshow(data['ux']['xspace'].real, vmin=-1, vmax=1)
+    P.imshow(data['ux']['xspace'].real)#, vmin=-1, vmax=1)
     P.colorbar()
     P.subplot(122)
-    P.imshow(data['uy']['xspace'].real, vmin=-1, vmax=1)
+    P.imshow(data['uy']['xspace'].real)#, vmin=-1, vmax=1)
     P.colorbar()
     if not os.path.exists('frames'):
         os.mkdir('frames')
@@ -22,3 +22,29 @@ def print_energy(data):
         energy += (data[f]['xspace']*data[f]['xspace'].conj()).real
 
     print "energy: %10.5e" % energy.mean()
+
+def en_spec(data, it):
+    kk = na.zeros(data['ux'].data.shape)
+    for k in data['ux'].k.values():
+        kk += k**2
+    kk = na.sqrt(kk)
+    power = na.zeros(data['ux'].data.shape)
+    for f in data.fields:
+        power += (data[f]['kspace']*data[f]['kspace'].conj()).real
+
+    nbins = (data['ux'].k['x'].size)/2 
+    k = na.arange(nbins)
+    spec = na.zeros(nbins)
+    for i in range(nbins):
+        spec[i] = (power[(kk >= (i-1/2.)) & (kk <= (i+1/2.))]).sum()
+
+    P.loglog(k,spec)
+    P.xlabel(r"$k$")
+    P.ylabel(r"$E(k)$")
+
+    if not os.path.exists('frames'):
+        os.mkdir('frames')
+    outfile = "frames/enspec_%04i.png" % it
+    P.savefig(outfile)
+    P.clf()
+
