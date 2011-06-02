a8de6fcd994547355691fd696fc1113ca1044c2c
J.S. Oishi <jsoishi@gmail.com>
2011-06-02 15:50:11 -0700

    added turbulence generator and one sample spectrum function

--- python/init_cond.py
+++ python/init_cond.py
@@ -1,3 +1,5 @@
+import numpy as na
+
 def taylor_green(ux, uy):
     if ux.dim == 2:
         ux.data[1,1] = -1j/4.
@@ -18,3 +20,25 @@ def sin_y(f,ampl=1.):
     f.data[1,0] = ampl*1j
     f.data[-1,0] = -f.data[0,1]
 
+def turb(ux, uy, spec, **kwargs):
+    """generate noise with a random phase and a spectrum given by
+    the spec function.
+
+    """
+    kk = na.zeros(ux.data.shape)
+    for k in ux.k.values():
+        kk += k**2
+    kk = na.sqrt(kk)
+    ampl = na.sqrt(spec(kk,**kwargs)/ux.data.ndim)
+    
+    for u in [ux,uy]:
+        u.data[:,:] = na.random.random(u.data.shape) + 0j
+        u._curr_space='xspace'
+        u.data[:,:] = ampl*u['kspace']
+
+def mcwilliams_spec(k, k0, ampl):
+    print "ampl = %10.5f" % ampl
+    spec = k**6./(k + 2.*k0)**18.
+    spec *= ampl/spec.sum()
+    return spec
+        
