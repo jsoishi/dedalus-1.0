d9089c9a99147928a32f11e176c387e720e24c52
J.S. Oishi <jsoishi@gmail.com>
2011-06-16 19:27:03 -0700

    sligtly better ICs for turbulence. still not returning real values.

--- python/init_cond.py
+++ python/init_cond.py
@@ -37,33 +37,19 @@ def turb(ux, uy, spec, **kwargs):
     ampl = na.sqrt(spec(kk,**kwargs)/ux.data.ndim)
     
     for u in [ux,uy]:
-        ## create random numbers in real space, transform to
-        ## complex. this ensures the field will be real, but screws up
-        ## the power spectrum.
-        #u.data[:,:] = na.random.random(u.data.shape) + 0j
-        #u._curr_space='xspace'
-        #u.data[:,:] = ampl*u['kspace']
-
-
-        ## this is the propoer way to do it, but needs to be filtered
-        ## to ensure it's real after ifft. currently, that doesn't
-        ## work....
-        # tantheta = na.tan(2*na.pi*na.random.random(u.data.shape))
-        # c = na.sqrt(ampl**2/(1.+tantheta**2))
-        # d = c*tantheta
-        # u.data[:,:] = c + 1j*d
-
-        u.data[:,:] = na.random.random(u.data.shape) +1j*na.random.random(u.data.shape)
+        u.data[:,:] = ampl*na.exp(1j*2*na.pi*na.random.random(u.data.shape))
 
         # enforce symmetry in kspace to ensure data is real in
         # xspace...doesn't work for 2D data yet
-
         nx = u.data.shape[0]
-        u.data[nx/2+1:,:] = u.data[nx/2-1:0:-1,:].conj()
+        u.data[-1:nx/2:-1,-1:nx/2:-1] = u.data[1:nx/2,1:nx/2].conj()
+        u.data[nx/2-1:0:-1, -1:nx/2:-1] = u.data[nx/2+1:,1:nx/2].conj()
+
         u.data[nx/2,:].imag = 0.
         u.data[:,nx/2].imag = 0.
         u.data[0,:].imag = 0.
         u.data[:,0].imag = 0.
+
 
 def mcwilliams_spec(k, k0, ampl):
     spec = k**6./(k + 2.*k0)**18.
