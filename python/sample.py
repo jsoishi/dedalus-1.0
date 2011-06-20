05fa188f4a8e52f86c1afd2e349141bd2d2c3726
J.S. Oishi <jsoishi@gmail.com>
2011-06-19 21:06:44 -0700

    for shootout timing: no analysis, no printouts

--- python/sample.py
+++ python/sample.py
@@ -2,9 +2,8 @@ from physics import Hydro
 from fourier_data import FourierData
 from time_step import RK2simple,RK2simplevisc
 from init_cond import taylor_green
-from analysis import AnalysisSet
 
-shape = (100,100) #(128,128,128)
+shape = (100,100) 
 RHS = Hydro(shape, FourierData)
 RHS.parameters['nu'] = 0.1
 data = RHS.create_fields(0.)
@@ -14,23 +13,10 @@ ti = RK2simplevisc(RHS,CFL=0.4)
 ti.stop_time(1.) # set stoptime
 ti.stop_iter(100) # max iterations
 ti.stop_walltime(3600.) # stop after 10 hours
-#an = Analysis(RHS)
-
-an = AnalysisSet(data, ti)
-an.add("print_energy", 1)
-an.add("snapshot", 100)
 
 #main loop
 dt = 1e-3
-#snapshot(data,0)
-an.run()
 while ti.ok:
-    print "step: %i" % ti.iter
+    #print "step: %i" % ti.iter
     ti.advance(data, dt)
-    an.run()
-    #print "ux (min, max) = (%10.5e, %10.5e, %10.5e, %10.5e)" % ((data['ux']['xspace'].real).min(),(data['ux']['xspace'].imag).min(), (data['ux']['xspace'].real).max(), (data['ux']['xspace'].imag).max())
-    #print "uy (min, max) = (%10.5e, %10.5e, %10.5e, %10.5e)" % ((data['uy']['xspace'].real).min(),(data['uy']['xspace'].imag).min(), (data['uy']['xspace'].real).max(), (data['uy']['xspace'].imag).max())
-
-    #an.chk() # see if it's time for analysis
-
 ti.final_stats()
