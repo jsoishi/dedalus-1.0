6e944efce52227e72f9edbded5fcdf7d8186a638
J.S. Oishi <jsoishi@gmail.com>
2011-06-20 10:14:00 -0700

    added mods.py

---
+++ dedalus/mods.py
@@ -0,0 +1,43 @@
+"""Conveninece wrapper for all tools necessary for a simulation.
+
+Author: J. S. Oishi <jsoishi@gmail.com>
+Affiliation: KIPAC/SLAC/Stanford
+License:
+  Copyright (C) 2011 J. S. Oishi.  All Rights Reserved.
+
+  This file is part of dedalus.
+
+  dedalus is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from dedalus.analysis.api import AnalysisSet
+
+from dedalus.data_objects.api import FourierData
+
+from dedalus.init_cond.api import \
+    taylor_green, \
+    sin_x, \
+    sin_y, \
+    turb,  \
+    mcwilliams_spec
+
+from dedalus.physics.api import Hydro
+
+from dedalus.time_step.api import \
+        RK2simple,\
+    RK2simplevisc, \
+    RK2simplehypervisc4
+
+
