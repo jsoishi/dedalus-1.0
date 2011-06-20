1feafcef5db1a339f8910c385d8d3f753ec6852e
J.S. Oishi <jsoishi@gmail.com>
2011-06-19 20:57:51 -0700

    adjusted parameters a bit. this version seems to work ok.

--- python/test_mcwilliams.py
+++ python/test_mcwilliams.py
@@ -6,21 +6,21 @@ from analysis import AnalysisSet
 
 shape = (450,450)
 RHS = Hydro(shape, FourierData)
-RHS.parameters['nu'] = 3.5e-8 # 10x mcwilliams
+RHS.parameters['nu'] = 3.5e-7 # 100x mcwilliams
 data = RHS.create_fields(0.)
 
 turb(data['ux'],data['uy'],mcwilliams_spec,k0=23.)
 ti = RK2simplehypervisc4(RHS,CFL=0.4)
 ti.stop_time(1.) # set stoptime
-ti.stop_iter(1) # max iterations
+ti.stop_iter(100) # max iterations
 ti.stop_walltime(3600.) # stop after 10 hours
 
 an = AnalysisSet(data, ti)
 an.add("print_energy", 1)
-an.add("field_snap", 2)
-an.add("en_spec",1)
+an.add("field_snap", 10)
+an.add("en_spec",5)
 #main loop
-dt = 1e-3
+dt = 2.5e-3
 #snapshot(data,0)
 an.run()
 while ti.ok:
