33daff989ccafa59b73275f49c625cb610082fc2
J.S. Oishi <jsoishi@gmail.com>
2011-06-23 11:26:04 -0700

    added import

--- dedalus/funcs.py
+++ dedalus/funcs.py
@@ -20,7 +20,7 @@ License:
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+import inspect
 __header = """
 == Welcome to the embedded IPython Shell ==
 
