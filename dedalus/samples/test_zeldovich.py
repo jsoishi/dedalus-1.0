74eb3e2261d002032eb0ff686911d2b4ee52392e
Greg Peairs <peaigr@gmail.com>
2011-07-21 13:08:30 -0700

    corrections to zeldovich analytic solution

--- dedalus/samples/test_zeldovich.py
+++ dedalus/samples/test_zeldovich.py
@@ -7,6 +7,21 @@ RHS = CollisionlessCosmology(shape, FourierRepresentation)
 data = RHS.create_fields(0.)
 H_0 = 2.27826587e-18 # 70.3 km/s/Mpc in seconds^-1
 ampl = 4e-19 # amplitude of initial velocity wave
+
+L = 2*na.pi # size of box
+N_p = 128 # resolution of analytic solution
+q = na.array([i for i in xrange(0,N_p)]) * L / N_p
+k = 2*na.pi/L # wavenumber of initial velocity perturbation
+a_i = 0.002 # initial scale factor
+t0 = (2./3.)/H_0 # present age of E-dS universe
+t_init = (a_i**(3./2.)) * t0 # time at which a = a_i in this universe
+
+Ddot_i = (2./3.) * ((1./t0)**(2./3.)) * (t_init**(-1./3.)) / a_i
+A = ampl / a_i / Ddot_i
+a_cross = a_i / (A * k)
+print "a_cross = ", a_cross
+tcross = (a_cross**(3./2.))*t0
+
 RHS.parameters['Omega_r'] = 0#8.4e-5
 RHS.parameters['Omega_m'] = 1#0.276
 RHS.parameters['Omega_l'] = 0#0.724
@@ -14,20 +29,21 @@ RHS.parameters['H0'] = H_0
 zeldovich(data, ampl)
 
 Myr = 3.15e13 # 10^6 years in seconds
-tstop = Myr*4e3
+tstop = tcross
+dt = Myr*1e1
 
 ti = RK2simple(RHS)
 ti.stop_time(tstop)
 ti.set_nsnap(1000)
 ti.set_dtsnap(1e19)
-dt = Myr*1e1
-
-t_snapshots = []
 
 ddelta = []
 uu = []
 uk = []
 
+t_snapshots = []
+a_snapshots = []
+  
 an = AnalysisSet(data, ti)
 an.add("field_snap", 20)
 an.add("en_spec", 20)
@@ -35,9 +51,8 @@ an.add("en_spec", 20)
 i = 0
 #an.run()
 while ti.ok:
-    ti.advance(data, dt)
     print "step: ", i
-    if data.time % (tstop/20) == 0:
+    if i % 20 == 0:
         tmp = na.zeros_like(data['u'][0]['xspace'][0,0,:].real)
         tmp[:] = data['u'][0]['xspace'][0,0,:].real
 
@@ -51,8 +66,11 @@ while ti.ok:
         ddelta.append(tmp2)
         uk.append(tmp3)
         t_snapshots.append(data.time)
+        a_snapshots.append(RHS.aux_eqns['a'].value)
+    ti.advance(data, dt)
     #an.run()
     i = i + 1
+print "a_stop = ", RHS.aux_eqns['a'].value
 
 def reorder(arr):
     di = len(arr)/2
@@ -69,29 +87,23 @@ for i in xrange(len(uu)):
 
 pl.figure()
 for i in xrange(len(uk)):
-    pl.plot(reorder(uk[i][(len(uk[i])/2):]),hold=True)
-
-L = 64 # size of box
-N_p1 = 64 # number of parallel sheets of particles
-q = na.array([i for i in xrange(0,64)]) * L / N_p1
-k = na.pi/32
-a_i = 0.002 # initial scale factor
-H_0 = 2.27826587e-18 # 70.3 km/s/Mpc in seconds^-1
-t0 = (2./3.)/H_0 # present age of E-dS universe
-ti = (a_i**(3./2.)) * t0 # time at which a = a_i in this universe
-
-Ddot_i = (2./3) * ((1./t0)**(2./3)) * (ti**(-1./3))
-A = ampl / a_i / Ddot_i
-a_cross = 1 / (A * k)
-tcross = (a_cross**(3./2.))*t0
+    pl.plot(reorder(uk[i])[(len(uk[i])/2):],hold=True)
 
 pl.figure()
 for t in t_snapshots:
+    t = t + t_init
     a = (t/t0)**(2./3)
-    D = a
-    x = q - D*A*na.sin(k*q)
-    Ddot = (2./3) * ((1./t0)**(2./3)) * (t**(-1./3))
-    delta = 1./(1.+D*A*k*na.cos(k*x))-1.
-    v = a * Ddot * A * na.sin(k*x)
-    pl.plot(v, hold=True)
+    D = a / a_i
+    x = q + D*A*na.sin(k*q)
+    Ddot = (2./3.) * ((1./t0)**(2./3.)) * (t**(-1./3.)) / a_i
+    delta = 1./(1.+D*A*k*na.cos(k*q))-1.
+    v = a * Ddot * A * na.sin(k*q)
+    phi = 3/2/a * ( (q**2 - x**2)/2 + 
+                    D * A * k * (k*q*na.sin(k*q) + na.cos(k*q) - 1) )
+    pl.plot(x, v, hold=True)
+pl.figure()
+pl.plot(x, v, hold=True)
+x = [2*na.pi*i/16. for i in xrange(16)]
+pl.plot(x, reorder(uu[-1]), '.', hold=True)
+
 pl.show()
