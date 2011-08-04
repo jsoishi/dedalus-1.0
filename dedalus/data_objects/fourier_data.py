410d15b349e6b4b21972bc2f4a5c3ab798a806d1
kburns <kburns@berkeley.edu>
2011-08-04 10:50:50 -0700

    Flag equality for dealiasing

--- dedalus/data_objects/fourier_data.py
+++ dedalus/data_objects/fourier_data.py
@@ -159,8 +159,8 @@ class FourierRepresentation(Representation):
         """Orszag 2/3 dealias rule"""
         
         # Zeroing mask   
-        dmask = ((na.abs(self.k['x']) > 2/3. * self.kny[-1]) | 
-                 (na.abs(self.k['y']) > 2/3. * self.kny[-2]))
+        dmask = ((na.abs(self.k['x']) >= 2/3. * self.kny[-1]) | 
+                 (na.abs(self.k['y']) >= 2/3. * self.kny[-2]))
 
         if self.ndim == 3:
             dmask = dmask | (na.abs(self.k['z']) >= 2/3. * self.kny[-3])
