74a3c8855138a678873998f38763ad9693b1e185
J.S. Oishi <jsoishi@gmail.com>
2011-09-13 08:47:41 -0700

    added 3D interface

--- dedalus/utils/fftw/_fftw.pyx
+++ dedalus/utils/fftw/_fftw.pyx
@@ -2,22 +2,52 @@ from _fftw cimport *
 
 cimport numpy as np
 import numpy as np
+fftw_flags = {'FFTW_FORWARD': FFTW_FORWARD,
+              'FFTW_BACKWARD': FFTW_BACKWARD,        
+              'FFTW_MEASURE': FFTW_MEASURE,
+              'FFTW_DESTROY_INPUT': FFTW_DESTROY_INPUT,
+              'FFTW_UNALIGNED': FFTW_UNALIGNED,
+              'FFTW_CONSERVE_MEMORY': FFTW_CONSERVE_MEMORY,
+              'FFTW_EXHAUSTIVE': FFTW_EXHAUSTIVE,
+              'FFTW_PRESERVE_INPUT': FFTW_PRESERVE_INPUT,
+              'FFTW_PATIENT': FFTW_PATIENT,
+              'FFTW_ESTIMATE': FFTW_ESTIMATE
+              }
 
 cdef class Plan:
     cdef fftw_plan _fftw_plan
     cdef np.ndarray _data
-    
+    cdef int direction
+    cdef int flags
     def __cinit__(self, data, direction='FFTW_FORWARD', flags=['FFTW_MEASURE']):
+        if direction == 'FFTW_FORWARD':
+            self.direction = FFTW_FORWARD
+        else:
+            self.direction = FFTW_BACKWARD
+        for f in flags:
+            self.flags = self.flags | fftw_flags[f]
         self._data = data
         if len(data.shape) == 2:
             self._fftw_plan = fftw_plan_dft_2d(data.shape[0],
                                                data.shape[1],
                                                <complex *> self._data.data,
                                                <complex *> self._data.data,
-                                               FFTW_FORWARD,
-                                               FFTW_MEASURE)
-            if self._fftw_plan == NULL:
-                raise RuntimeError("FFTW could not create plan.")
+                                               self.direction,
+                                               self.flags)
+        elif len(data.shape) == 3:
+            self._fftw_plan = fftw_plan_dft_3d(data.shape[0],
+                                               data.shape[1],
+                                               data.shape[2],
+                                               <complex *> self._data.data,
+                                               <complex *> self._data.data,
+                                               self.direction,
+                                               self.flags)
+        else:
+            raise RuntimeError("Only 2D and 3D arrays are supported.")
+
+        if self._fftw_plan == NULL:
+            raise RuntimeError("FFTW could not create plan.")
+
             
     def __init__(self, data, direction='FFTW_FORWARD', flags=['FFTW_MEASURE']):
         pass
@@ -33,6 +63,5 @@ cdef class Plan:
         def __get__(self):
             return self._data
     
-
         def __set__(self, inp):
             self._data[:]=inp
