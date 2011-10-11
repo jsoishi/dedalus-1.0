2a08f9f8cad170e1357e0bd8fb271ec38208981b
J.S. Oishi <jsoishi@gmail.com>
2011-10-11 13:11:09 -0700

    removed symmetric normalization. this was causing problems in MHD, at least with FFTW.

--- dedalus/data_objects/fourier_data.py
+++ dedalus/data_objects/fourier_data.py
@@ -49,7 +49,7 @@ class FourierRepresentation(Representation):
     """
 
     def __init__(self, sd, shape, length, dtype='complex128', method='fftw',
-                 dealiasing='2/3'):
+                 dealiasing='2/3 cython'):
         """
         Inputs:
             shape       The shape of the data, tuple of ints
@@ -151,18 +151,17 @@ class FourierRepresentation(Representation):
 
     def fwd_fftw(self):
         self.fplan()
-        self.data /= na.sqrt(self.data.size)
+        self.data /= self.data.size
 
     def rev_fftw(self):
         self.rplan()
-        self.data /= na.sqrt(self.data.size)
         self.data.imag = 0.
 
     def fwd_np(self):
-        self.data = fpack.fftn(self.data / na.sqrt(self.data.size))
+        self.data = fpack.fftn(self.data)
 
     def rev_np(self):
-        self.data = fpack.ifftn(self.data) * na.sqrt(self.data.size)
+        self.data = fpack.ifftn(self.data)
         self.data.imag = 0
 
     def forward(self):
