63e6bdf0cc3a6523a2e74488e31d5f85e78b0806
J.S. Oishi <jsoishi@gmail.com>
2011-11-01 10:38:59 -0700

    added first draft of setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+# import distribute_setup
+# distribute_setup.use_setuptools()
+# import setuptools
+
+def find_fftw():
+    import glob
+    import os
+    path = "/usr/lib"
+    l = glob.glob(os.path.join(path,"*fftw*"))
+    if len(l) != 0:
+        return path
+    path = os.path.expanduser("~/build/lib")
+    l = glob.glob(os.path.join(path,"*fftw*"))
+    if len(l) != 0:
+        return path
+
+setup(
+    name='dedalus',
+    version='0.1dev',
+    author='J. S. Oishi',
+    author_email='jsoishi@gmail.com',
+    license='GPL3',
+    packages = [#'dedalus.analysis',
+                #'dedalus.data_objects',
+                'dedalus'],
+                #'dedalus.init_cond',
+                #'dedalus.physics',
+                #'dedalus.time_stepping',
+                #'dedalus.utils'],
+    include_dirs = [np.get_include()],
+    #cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("dedalus.utils.fftw", ["dedalus/utils/fftw/_fftw.c"],
+                             libraries=["fftw3"],
+                             library_dirs=[find_fftw()])]
+    
+    )
