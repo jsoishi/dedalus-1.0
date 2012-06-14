5345698b89d7e9e5e338054d1e9b053e6d76c670
J.S. Oishi <jsoishi@gmail.com>
2012-06-13 19:47:36 -0700

    normalized properly

--- dedalus/init_cond/turb_spectra.py
+++ dedalus/init_cond/turb_spectra.py
@@ -1,6 +1,9 @@
-def mcwilliams_spec(k, k0):
+def mcwilliams_spec(k, k0, E0):
     """spectrum from McWilliams (1990), JFM 219:361-385
 
     """
     spec = k**6./(k + 2.*k0)**18.
+    # normalize by 1D spectrum
+    sl = (slice(0,k.shape[0]-1),slice(0,1))
+    spec *= E0/spec[sl].sum()
     return spec
