604a4bb789dba70e27ac0b7783f7b88787e3c1b6
J.S. Oishi <jsoishi@gmail.com>
2012-06-19 17:50:07 -0700

    need to broadcast the normalization factor in parallel.

--- dedalus/init_cond/turb_spectra.py
+++ dedalus/init_cond/turb_spectra.py
@@ -1,9 +1,15 @@
+from dedalus.utils.logger import mylog
+from dedalus.utils.parallelism import com_sys
 def mcwilliams_spec(k, k0, E0):
     """spectrum from McWilliams (1990), JFM 219:361-385
 
     """
     spec = k**6./(k + 2.*k0)**18.
     # normalize by 1D spectrum
-    sl = (slice(0,k.shape[0]-1),slice(0,1))
-    spec *= E0/spec[sl].sum()
-    return spec
+    if com_sys.myproc == 0:
+        sl = (slice(0,1),slice(0,k.shape[1]/2-1))
+        norm = E0/spec[sl].sum()
+    else:
+        norm = 0.
+    com_sys.comm.bcast(norm, root=0)
+    return spec*norm
