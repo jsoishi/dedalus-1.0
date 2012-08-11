3093cfabf33a8c3f64d959ef1c89493e39bbc001
J.S. Oishi <jsoishi@gmail.com>
2012-08-11 16:01:07 -0700

    added comments.

--- samples/hydro/boussinesq/gravity_wave/2d_gmode_kx1_kz1.py
+++ samples/hydro/boussinesq/gravity_wave/2d_gmode_kx1_kz1.py
@@ -1,3 +1,27 @@
+"""
+This is a 2D gravity mode test. It sets up an exact eigenmode of the
+gravity wave, and evolves it for two wave periods.
+
+Author: J. S. Oishi <jsoishi@gmail.com>
+Affiliation: KIPAC/SLAC/Stanford
+License:
+  Copyright (C) 2012 J. S. Oishi.  All Rights Reserved.
+
+  This file is part of dedalus.
+
+  dedalus is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from dedalus.mods import *
 from dedalus.funcs import insert_ipython
 import numpy as np
