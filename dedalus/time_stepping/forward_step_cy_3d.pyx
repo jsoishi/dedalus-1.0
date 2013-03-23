fa06c9354fc17b466a75c3ca7859801cb484ac8b
Keaton J. Burns <keaton.burns@gmail.com>
2013-03-23 20:22:54 +0000

    Add Taylor series evaluations to 3D ETD time stepping schemes, vastly improving accuracy for low values of IF * dt.  Other optimizations include removing None checking, type asserts, and divide-by-zero warnings.

--- dedalus/time_stepping/forward_step_cy_3d.pyx
+++ dedalus/time_stepping/forward_step_cy_3d.pyx
@@ -1,26 +1,28 @@
+
+
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: nonecheck=False
+#cython: cdivision=True
+
+
 import numpy as np
 cimport numpy as np
-cimport cython
 from libc.math cimport exp
 
-DTYPE1 = np.complex128
-ctypedef np.complex128_t DTYPE1_t
 
-DTYPE2 = np.float64
-ctypedef np.float64_t DTYPE2_t
+nc128 = np.complex128
+ctypedef np.complex128_t nc128_t
+
+nf64 = np.float64
+ctypedef np.float64_t nf64_t
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def euler(np.ndarray[DTYPE1_t, ndim=3] start not None,
-          np.ndarray[DTYPE1_t, ndim=3] output not None,
-          np.ndarray[DTYPE1_t, ndim=3] deriv not None,
-          DTYPE2_t dt):
+def euler(np.ndarray[nc128_t, ndim=3] start,
+          np.ndarray[nc128_t, ndim=3] output,
+          np.ndarray[nc128_t, ndim=3] deriv,
+          nf64_t dt):
     """First-order forward-Euler step."""
-
-    assert start.dtype == DTYPE1
-    assert output.dtype == DTYPE1
-    assert deriv.dtype == DTYPE1
 
     cdef int xmax = start.shape[2]
     cdef int ymax = start.shape[0]
@@ -33,99 +35,124 @@ def euler(np.ndarray[DTYPE1_t, ndim=3] start not None,
                 output[y, z, x] = start[y, z, x] + dt * deriv[y, z, x]
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def etd1(np.ndarray[DTYPE1_t, ndim=3] start not None,
-         np.ndarray[DTYPE1_t, ndim=3] output not None,
-         np.ndarray[DTYPE1_t, ndim=3] deriv not None,
-         np.ndarray[DTYPE2_t, ndim=3] intfactor not None,
-         DTYPE2_t dt):
+def etd1(np.ndarray[nc128_t, ndim=3] start,
+         np.ndarray[nc128_t, ndim=3] output,
+         np.ndarray[nc128_t, ndim=3] deriv,
+         np.ndarray[nf64_t, ndim=3] intfactor,
+         nf64_t dt):
     """First-order ETD step."""
 
-    assert start.dtype == DTYPE1
-    assert output.dtype == DTYPE1
-    assert deriv.dtype == DTYPE1
-    assert intfactor.dtype == DTYPE2
-
     cdef int xmax = start.shape[2]
     cdef int ymax = start.shape[0]
     cdef int zmax = start.shape[1]
     cdef unsigned int x, y, z
-    cdef DTYPE2_t c, eif
+    cdef nf64_t Z, f0, f1
 
     for y in range(ymax):
         for z in range(zmax):
             for x in range(xmax):
-                if intfactor[y, z, x] == 0.:
+                Z = intfactor[y, z, x] * dt
+                if Z == 0.:
                     output[y, z, x] = start[y, z, x] + dt * deriv[y, z, x]
                 else:
-                    c = intfactor[y, z, x]
-                    eif = exp(c * dt)
-                    output[y, z, x] = start[y, z, x] * eif + (eif - 1.) * deriv[y, z, x] / c
+                    if ((Z < 1.) and (Z > -1.)):
+                        f0 = f_taylor(0, Z)
+                        f1 = f_taylor(1, Z)
+                    else:
+                        f0 = exp(Z)
+                        f1 = (f0 - 1.) / Z
+                    output[y, z, x] = start[y, z, x] * f0 + deriv[y, z, x] * f1 * dt
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def etd2rk1(np.ndarray[DTYPE1_t, ndim=3] start not None,
-            np.ndarray[DTYPE1_t, ndim=3] output not None,
-            np.ndarray[DTYPE1_t, ndim=3] deriv1 not None,
-            np.ndarray[DTYPE1_t, ndim=3] deriv2 not None,
-            np.ndarray[DTYPE2_t, ndim=3] intfactor not None,
-            DTYPE2_t dt):
+def etd2rk1(np.ndarray[nc128_t, ndim=3] start,
+            np.ndarray[nc128_t, ndim=3] output,
+            np.ndarray[nc128_t, ndim=3] deriv1,
+            np.ndarray[nc128_t, ndim=3] deriv2,
+            np.ndarray[nf64_t, ndim=3] intfactor,
+            nf64_t dt):
     """Second step of ETD2RK1."""
 
-    assert start.dtype == DTYPE1
-    assert output.dtype == DTYPE1
-    assert deriv1.dtype == DTYPE1
-    assert deriv2.dtype == DTYPE1
-    assert intfactor.dtype == DTYPE2
-
     cdef int xmax = start.shape[2]
     cdef int ymax = start.shape[0]
     cdef int zmax = start.shape[1]
     cdef unsigned int x, y, z
-    cdef DTYPE2_t c, eif
+    cdef nf64_t Z, f0, f1, f2
 
     for y in range(ymax):
         for z in range(zmax):
             for x in range(xmax):
-                if intfactor[y, z, x] == 0.:
+                Z = intfactor[y, z, x] * dt
+                if Z == 0.:
                     output[y, z, x] = start[y, z, x] + dt / 2. * (deriv2[y, z, x] - deriv1[y, z, x])
                 else:
-                    c = intfactor[y, z, x]
-                    eif = exp(c * dt)
-                    output[y, z, x] = start[y, z, x] + (eif - c * dt - 1.) * (deriv2[y, z, x] - deriv1[y, z, x]) / (c * c * dt)
+                    if ((Z < 1.) and (Z > -1.)):
+                        f0 = f_taylor(0, Z)
+                        f1 = f_taylor(1, Z)
+                        f2 = f_taylor(2, Z)
+                    else:
+                        f0 = exp(Z)
+                        f1 = (f0 - 1.) / Z
+                        f2 = (f1 - 1.) / Z
+                    output[y, z, x] = start[y, z, x] + (deriv2[y, z, x] - deriv1[y, z, x]) * f2 * dt
 
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-def etd2rk2(np.ndarray[DTYPE1_t, ndim=3] start not None,
-            np.ndarray[DTYPE1_t, ndim=3] output not None,
-            np.ndarray[DTYPE1_t, ndim=3] deriv1 not None,
-            np.ndarray[DTYPE1_t, ndim=3] deriv2 not None,
-            np.ndarray[DTYPE2_t, ndim=3] intfactor not None,
-            DTYPE2_t dt):
+def etd2rk2(np.ndarray[nc128_t, ndim=3] start,
+            np.ndarray[nc128_t, ndim=3] output,
+            np.ndarray[nc128_t, ndim=3] deriv1,
+            np.ndarray[nc128_t, ndim=3] deriv2,
+            np.ndarray[nf64_t, ndim=3] intfactor,
+            nf64_t dt):
     """Second step of ETD2RK2."""
-
-    assert start.dtype == DTYPE1
-    assert output.dtype == DTYPE1
-    assert deriv1.dtype == DTYPE1
-    assert deriv2.dtype == DTYPE1
-    assert intfactor.dtype == DTYPE2
 
     cdef int xmax = start.shape[2]
     cdef int ymax = start.shape[0]
     cdef int zmax = start.shape[1]
     cdef unsigned int x, y, z
-    cdef DTYPE2_t c, eif
+    cdef nf64_t Z, f0, f1, f2
 
     for y in range(ymax):
         for z in range(zmax):
             for x in range(xmax):
-                if intfactor[y, z, x] == 0.:
+                Z = intfactor[y, z, x] * dt
+                if Z == 0.:
                     output[y, z, x] = start[y, z, x] + dt * deriv2[y, z, x]
                 else:
-                    c = intfactor[y, z, x]
-                    eif = exp(c * dt)
-                    output[y, z, x] = start[y, z, x] * eif + (((c * dt - 2.) * eif + c * dt + 2.) * deriv1[y, z, x] + (2 * (eif - c * dt - 1.)) * deriv2[y, z, x]) / (c * c * dt)
+                    if ((Z < 1.) and (Z > -1.)):
+                        f0 = f_taylor(0, Z)
+                        f1 = f_taylor(1, Z)
+                        f2 = f_taylor(2, Z)
+                    else:
+                        f0 = exp(Z)
+                        f1 = (f0 - 1.) / Z
+                        f2 = (f1 - 1.) / Z
+                    output[y, z, x] = start[y, z, x] * f0 + (deriv2[y, z, x] - deriv1[y, z, x]) * 2. * f2 * dt + deriv1[y, z, x] * f1 * dt
+
+
+cdef nf64_t f_taylor(int k, nf64_t Z):
+    """
+    Truncated Taylor series approximations to f.
+
+    Notes
+    -----
+         m
+    f = sum Z ** (j - k) / j!
+        j=k
+
+    """
+
+    cdef int j
+    cdef int m = 20
+    cdef nf64_t factj = 1.
+    cdef nf64_t sum = 0.
+
+    if k > 2:
+        for j in range(1, k):
+            factj *= j
+
+    for j in range(k, m):
+        if j > 1:
+            factj *= j
+        sum += Z ** (j - k) / factj
+
+    return sum
 
